2d77219bfeaad08d32f23b540bc77c955cabaaaf
AyushPatel_03 <126902793+03ayush09@users.noreply.github.com>
2025-07-11 00:00:11 +0530

    .env updated

--- client/src/views/About/index.tsx
+++ client/src/views/About/index.tsx
@@ -50,12 +50,12 @@ const About = () => {
             >
               <Box sx={{ width: "75px", height: "75px" }}>
                 <img
-                  src="https://firebasestorage.googleapis.com/v0/b/real-estate-54ca1.appspot.com/o/1701424117897salman%20passport.jpg?alt=media&token=a903aba6-b78e-4442-b0c0-f07c0dfa145f"
-                  alt="salman"
+                  src=""
+                  alt="lmn"
                   style={{ width: "100%", height: "100%", borderRadius: "50%" }}
                 />
               </Box>
-              <SubHeading sx={{ marginTop: "10px" }}>Salman Muazam</SubHeading>
+              <SubHeading sx={{ marginTop: "10px" }}>lmn</SubHeading>
               <Box>0323 4910955</Box>
               <Heading sx={{ fontSize: "14px", marginTop: "10px" }}>
                 CEO
@@ -71,12 +71,12 @@ const About = () => {
             >
               <Box sx={{ width: "75px", height: "75px" }}>
                 <img
-                  src="https://firebasestorage.googleapis.com/v0/b/real-estate-54ca1.appspot.com/o/1701198186809Hassan.jpg?alt=media&token=881f2f1b-b0d4-4933-9b4a-79259d313f42"
-                  alt="hassan"
+                  src=""
+                  alt="pqr"
                   style={{ width: "100%", height: "100%", borderRadius: "50%" }}
                 />
               </Box>
-              <SubHeading sx={{ marginTop: "10px" }}>Hassan Raza</SubHeading>
+              <SubHeading sx={{ marginTop: "10px" }}>pqr</SubHeading>
               <Box>0300 0315440</Box>
               <Heading sx={{ fontSize: "14px", marginTop: "10px" }}>
                 President
@@ -93,11 +93,11 @@ const About = () => {
               <Box sx={{ width: "75px", height: "75px" }}>
                 <img
                   src="https://firebasestorage.googleapis.com/v0/b/real-estate-54ca1.appspot.com/o/1701198124445Faizan.jpg?alt=media&token=560ccfc3-f5f4-430c-b55d-d6a0357c7be2"
-                  alt="salman"
+                  alt="abc"
                   style={{ width: "100%", height: "100%", borderRadius: "50%" }}
                 />
               </Box>
-              <SubHeading sx={{ marginTop: "10px" }}>Ch Faizan</SubHeading>
+              <SubHeading sx={{ marginTop: "10px" }}>abc</SubHeading>
               <Box>0355 5032437</Box>
               <Heading sx={{ fontSize: "14px", marginTop: "10px" }}>
                 Marketing Manager
